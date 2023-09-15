fc58b54bdf9d9c00077f86249efb16df1623556b
catandladybug <98188215+catandladybug@users.noreply.github.com>
2023-09-14 19:49:11 -0700

    .

---
+++ Command/Assets/Scripts/Command.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace Chapter.Command
+{
+    public abstract class Command
+    {
+        public abstract void Execute();
+    }
+    public class ToggleTurbo : Command
+    {
+        private BikeController _controller;
+        public ToggleTurbo(BikeController controller)
+        {
+            _controller = controller;
+        }
+        public override void Execute()
+        {
+            _controller.ToggleTurbo();
+        }
+    }
+    public class TurnLeft : Command 
+    { 
+        private BikeController _controller; 
+        public TurnLeft(BikeController controller) 
+        { 
+            _controller = controller; 
+        } 
+        public override void Execute() 
+        { 
+            _controller.Turn(BikeController.Direction.Left); 
+        } 
+    }
+    public class TurnRight : Command 
+    { 
+        private BikeController _controller; 
+        public TurnRight(BikeController controller) 
+        { 
+            _controller = controller; 
+        } 
+        public override void Execute() 
+        { 
+            _controller.Turn(BikeController.Direction.Right); 
+        } 
+    }
+}
+
---
+++ Event Bus/Assets/Scripts/RaceEventType.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter.EventBus 
+{ 
+    public enum RaceEventType 
+    { 
+        COUNTDOWN, START, RESTART, PAUSE, STOP, FINISH, QUIT 
+    } 
+}
